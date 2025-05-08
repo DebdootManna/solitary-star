d609ab9511178f5b3028f54aa511b4e50f6922a2
Debdoot Manna <106653463+DebdootManna@users.noreply.github.com>
2025-05-08 19:02:57 +0530

    updating boomplay music

--- src/components/StreamingLinks.tsx
+++ src/components/StreamingLinks.tsx
@@ -58,7 +58,7 @@ const streamingPlatforms = [
     name: 'Boomplay',
     icon: <Radio className="w-5 h-5" />,
     color: '#E83C38',
-    url: '<iframe src='https://www.boomplay.com/embed/106678978/COL',
+    url: 'https://www.boomplay.com/albums/106678978',
   },
 ];
 
